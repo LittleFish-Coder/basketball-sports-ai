8ba8809c763d14445b39f83d4a41915700a37381
LittleFish-Coder <adrianyu890317@gmail.com>
2023-12-26 02:53:28 +0800

    modify ball tracking test.py

--- ball_tracking/test.py
+++ ball_tracking/test.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 import os
 
-model = YOLO() 
-pt_path = os.path.join(os.getcwd(), 'ball_rimV5.pt')
-model = YOLO(pt_path) 
-results = model.predict('testVideos/back.mp4',conf=0.3, classes=[0,1], show=True, max_det=1,save=False) # basketball
+pt_path = os.path.join(os.getcwd(), "model_pt/ball_rimV5.pt")
+model = YOLO(pt_path)
 
+# show how many class in the model
+print(model.names)
 
-
+# inference
+results = model.predict("testing-datasets/side.mp4", conf=0.3, save=True)  # basketball
